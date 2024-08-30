89dc823b9854d393fa37049a771a6555c0a5ce2e
ChanchalNarang90 <smile.narang1990@gmail.com>
2024-08-30 13:25:31 +0530

    2 files forecasting wind speed

---
+++ latest-windpower.py
@@ -0,0 +1,16 @@
+pip install python
+import pandas as pd
+import numpy as np
+from pandas import DatetimeIndex
+TurbineData=pd.read_csv('Turbine_Data.csv')
+Turbine_df =pd.DataFrame(TurbineData)
+df=Turbine_df.isna().sum()
+Turbine_df=Turbine_df.dropna()
+df1=Turbine_df.isna().sum()
+CleanData=Turbine_df 
+Clean_df=pd.DataFrame(CleanData)
+print(Clean_df.isna().sum())
+print(Clean_df)
+
+
+ll
