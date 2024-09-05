d59e07102e7b714ec22fb40c7a716c00891f5133
chanchalnarang90 <narangchanchal4@gmail.com>
2024-09-05 22:00:26 +0530

    project1 code till cleaning

--- latest-windpower.py
+++ latest-windpower.py
@@ -1,15 +1,23 @@
 import pandas as pd
-import numpy as np
-from pandas import DatetimeIndex
 TurbineData=pd.read_csv('Turbine_Data.csv')
 Turbine_df =pd.DataFrame(TurbineData)
 df=Turbine_df.isna().sum()
 Turbine_df=Turbine_df.dropna()
-df1=Turbine_df.isna().sum()
-CleanData=Turbine_df 
-Clean_df=pd.DataFrame(CleanData)
-print(Clean_df.isna().sum())
-print(Clean_df)
+Turbine_df.isna().sum()
+Turbine_df['ActivePower']= [ value  if value>0 else 0 for value in Turbine_df['ActivePower'] ]
+print(Turbine_df)
+
+
+# CleanData=Turbine_df 
+# Clean_df=pd.DataFrame(CleanData)
+# Clean_df_updated=[pd.to_datetime(row) for row in Clean_df['Unnamed: 0']]
+# updated_Df=pd.DataFrame(Clean_df_updated)
+# print(updated_Df.columns)
+
+
+
+
+
  
 
 
