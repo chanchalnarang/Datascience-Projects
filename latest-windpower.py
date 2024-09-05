36e63a8db6d0fc51b73b18e677c3737b7ad1ba7a
chanchalnarang90 <narangchanchal4@gmail.com>
2024-09-05 22:40:30 +0530

    small changes

--- latest-windpower.py
+++ latest-windpower.py
@@ -4,15 +4,7 @@ Turbine_df =pd.DataFrame(TurbineData)
 df=Turbine_df.isna().sum()
 Turbine_df=Turbine_df.dropna()
 Turbine_df.isna().sum()
-Turbine_df['ActivePower']= [ value  if value>0 else 0 for value in Turbine_df['ActivePower'] ]
-print(Turbine_df)
-
-
-# CleanData=Turbine_df 
-# Clean_df=pd.DataFrame(CleanData)
-# Clean_df_updated=[pd.to_datetime(row) for row in Clean_df['Unnamed: 0']]
-# updated_Df=pd.DataFrame(Clean_df_updated)
-# print(updated_
+Turbine_df['ActivePower']= [ value  if value>0 else 0 for value in Turbine_df['ActivePower'] ]        
 
 
 
